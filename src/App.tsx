27d7cfcc3345ce29462879feb5d95ceeba9bf157
Fedde Tilma <feddetilma@hotmail.com>
2023-05-23 16:57:08 +0200

    pseudo code for todos

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,18 @@ import React from 'react';
 import './App.scss';
 
 const App = () => {
+  // get customers from 'API' and store
+
+  // get password list from local storage
+
+  // component
+  // input for password, title, customerName
+
+  // display all passwords, password itself hidden
+  // match customerName with color
+  // toggle to display password
+
+
   return (
     <div>
       Hello
