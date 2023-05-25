74a76cbadcd4b85237a2cd54088f8795100e1e9f
Fedde Tilma <feddetilma@hotmail.com>
2023-05-25 09:43:47 +0200

    stub customers data for e2e tests

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -1,20 +1,19 @@
-// ***********************************************************
-// This example support/e2e.ts is processed and
-// loaded automatically before your test files.
-//
-// This is a great place to put global configuration and
-// behavior that modifies Cypress.
-//
-// You can change the location of this file or turn off
-// automatically serving support files with the
-// 'supportFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/configuration
-// ***********************************************************
-
 // Import commands.js using ES2015 syntax:
 import './commands'
 
-// Alternatively you can use CommonJS syntax:
-// require('./commands')
+beforeEach(() => {
+	// for now to avoid issues with cors / rate limit api.allorigins. Mock data customers
+	cy.intercept('https://api.allorigins.win/get?url=https://pastebin.com/raw/zSFTiVWr',
+
+		{
+			"contents": "[\r\n  {\r\n    \"name\": \"Payroll Select\",\r\n    \"color\": \"#0000FF\"\r\n  },\r\n  {\r\n    \"name\": \"Benu Direct\",\r\n    \"color\": \"#259617\"\r\n  },\r\n  {\r\n    \"name\": \"Schotpoort Connect\",\r\n    \"color\": \"#e3c922\"\r\n  },\r\n  {\r\n    \"name\": \"KNHB\",\r\n    \"color\": \"#e39c22\"\r\n  }\r\n]",
+			"status": {
+				"url": "https://pastebin.com/raw/zSFTiVWr",
+				"content_type": "text/plain; charset=utf-8",
+				"http_code": 200,
+				"response_time": 37,
+				"content_length": 258
+			}
+		}
+	);
+});
